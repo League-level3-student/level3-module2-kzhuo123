18f9f3a20202032b2645bac9acb93ccc116585ce
League Student <league@iMac-18.attlocal.net>
2019-09-18 19:55:25 -0700

    :ear:
    dfgsdfgsdfg

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -4,30 +4,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import _00_Sorting_Algorithms._00_SortedArrayChecker;
+
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
-	
+
 	@Test
 	public void testLinearSearch() {
-		//1. use the assertEquals method to test your linear search method.
+		// 1. use the assertEquals method to test your linear search method.
+		String[] sorted = { "a", "b", "bob", "chad", "kevin", "shrek" };
+		assertEquals("kevin", sorted[4]);
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] equals= {0, 1 ,2,3,4,5,6,7,8,9,10};
+		
 	}
-	
+
 	@Test
 	public void testInterpolationSearch() {
-		//3. use the assertEquals method to test your interpolation search method.
-		//   remember that the array must be sorted and evenly distributed
+		// 3. use the assertEquals method to test your interpolation search method.
+		// remember that the array must be sorted and evenly distributed
 	}
-	
+
 	@Test
 	public void testExponentialSearch() {
-		//4. use the assertEquals method to test your exponential search method.
-		//   remember that the array must be sorted
+		// 4. use the assertEquals method to test your exponential search method.
+		// remember that the array must be sorted
 	}
 }
